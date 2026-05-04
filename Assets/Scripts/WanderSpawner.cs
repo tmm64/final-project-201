48e356c8587018ef61f6f51ac645d941bf18f419
Tanib Momen <tmm64@njit.edu>
2026-05-03 23:39:35 -0400

    Added some new assets, created platforms and fixed instantiation

--- Assets/Scripts/WanderSpawner.cs
+++ Assets/Scripts/WanderSpawner.cs
@@ -3,15 +3,16 @@ using UnityEngine;
 public class WanderSpawner : MonoBehaviour
 {
     public GameObject prefab;
-    public float spawnDist = 10f;
-
+    public float spawnDist = 5f;
+ 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse1))
         {
             Vector3 spawnPos = transform.position + transform.forward * spawnDist;
-            Instantiate(prefab, spawnPos, Quaternion.identity);
+            GameObject spawned = Instantiate(prefab, spawnPos, Quaternion.identity);
+            Destroy(spawned, 5);
         }
     }
 }
