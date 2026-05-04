0f3364bc8c2f1c90af2426459264616b926203d2
Tanib <tmm64@njit.edu>
2026-05-04 15:24:27 -0400

    updated PlayerLogic script

--- Assets/Scripts/PlayerLogic.cs
+++ Assets/Scripts/PlayerLogic.cs
@@ -4,16 +4,15 @@ public class PlayerLogic : MonoBehaviour
 {   // Dash will be done by briefly turning on kinematics then turning it off again.
     // Jump will be a basic jump
     //Stomp is uhhhh basically the dash logic but downwards.
-    
+    private Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>(); 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   void extraJump(Rigidbody rb)
+   {
+       // Implement extra jump logic here
+   }
 }
